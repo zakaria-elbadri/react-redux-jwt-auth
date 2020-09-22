4182a78b5909e093189a9a7c715fffbad6b5d774
Zakaria EL BADRI <Zakaria.elBadri@ingenico.com>
2020-09-22 13:24:44 +0200

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
         <div>
           <nav className="navbar navbar-expand navbar-dark bg-dark">
             <Link to={"/"} className="navbar-brand">
-              React JWS example app
+              React JWS Example App
             </Link>
             <div className="navbar-nav mr-auto">
               <li className="nav-item">
